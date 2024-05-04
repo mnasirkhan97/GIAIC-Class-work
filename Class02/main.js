6109fcf047c6e917390f6333c318d7f477282457
M Nasir Khan <immnk97@gmail.com>
2024-05-04 23:50:49 +0500

    Day 02

---
+++ Class02/main.js
@@ -0,0 +1,33 @@
+// // // let favColor = "blue";
+// // let favColor = "green";
+// // console.log(favColor)
+// let favColor = "blue";
+// console.log(favColor);
+// favColor = "green";
+// console.log(favColor)
+// //case sensetive
+// // 1. camelCase
+// // 2. snake_case
+// // 3. ParcelCase
+// let userName : string = "Nasir Khan";
+// // userName = 26; // error
+// let fullName = "Muhammad Nasir khan";
+// console.log(fullName)
+// // Strong typing in typescript
+// let str : string = "Pakistan"
+// console.log(str) // "str" k variable me srf string he likhengy is k ilawa error de ga
+// // str = 19 // error
+var fullName = "nasir";
+console.log(fullName);
+fullName = "khan";
+console.log(fullName);
+var userName = "Muhammad Bilal";
+userName = "Ariba";
+console.log(userName);
+var age = 20;
+var New_age = 25; // strong typing => data type phly se bta dety hn
+console.log(age);
+console.log(New_age);
+var gravity = 9.8;
+// console.log(gravity)
+// gravity = 11 // error  (const ki value ko again value nh de skty)
---
+++ Class02/main.ts
@@ -0,0 +1,94 @@
+// // // let favColor = "blue";
+// // let favColor = "green";
+
+
+// // console.log(favColor)
+
+// let favColor = "blue";
+// console.log(favColor);
+// favColor = "green";
+// console.log(favColor)
+
+// //case sensetive
+
+// // 1. camelCase
+// // 2. snake_case
+// // 3. ParcelCase
+
+// let userName : string = "Nasir Khan";
+
+// // userName = 26; // error
+
+// let fullName = "Muhammad Nasir khan";
+
+// console.log(fullName)
+
+// // Strong typing in typescript
+
+// let str : string = "Pakistan"
+
+// console.log(str) // "str" k variable me srf string he likhengy is k ilawa error de ga
+
+// // str = 19 // error
+
+
+
+
+// let fullName = "nasir"
+// console.log(fullName);
+
+// fullName = "khan"
+// console.log(fullName);
+
+// let userName = "Muhammad Bilal";
+// userName = "Ariba"
+
+// console.log(userName)
+
+
+
+// let age = 20 
+// let New_age : number = 25 // strong typing => data type phly se bta dety hn
+
+// console.log(age)
+// console.log(New_age)
+
+
+// const gravity = 9.8;
+// // console.log(gravity)
+
+// // gravity = 11 // error  (const ki value ko again value nh de skty)
+
+
+let currentBalance = 100; // this value can change
+const accountNum = 23456567; // this value can not change
+
+currentBalance = 50;  // This is allowed..
+// accountNum = 3455456787;   // error this is not allowed..
+
+
+// =======> Additional Data type 
+
+// 1. undefined
+
+// 2. Unknown
+
+// 3. BigInt
+
+// 4. Symbol
+
+// 5. Null
+
+
+
+//======> Errors
+
+// Syntax error
+
+// lett message = "hello"  // syntax error
+// console.log(message) // not printed
+
+
+
+// type error  
+
