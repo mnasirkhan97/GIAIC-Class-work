4fa1f27994057f88b7b84a4885ec6757abc207f3
M Nasir Khan <immnk97@gmail.com>
2024-05-07 14:08:49 +0500

    Class 05

---
+++ Class05/main.js
@@ -0,0 +1,127 @@
+// Operators in TypeScript
+// ==========> Addition
+// console.log(2+3)
+// let num1 : number = 3;
+// let num2 : number = 2;
+// console.log(num1 + num2)
+// let num1 : number = 3;
+// let num2 : number = 2;
+// let card : number = num1 + num2 
+// console.log(card)
+//==========> Subtraction
+// console.log(5 - 3);
+// let num1 : number = 3;
+// let num2 : number = 2;
+// let card : number = num1 - num2 
+// console.log(card)
+//======> Multiplication
+// let num1 : number = 3;
+// let num2 : number = 2;
+// let card : number = num1 * num2 
+// console.log(card)
+// let bags : number = 4;
+// let apples : number = 5;
+// let sum : number = bags * apples 
+// console.log(sum);
+// =======> Divition
+// let num1 : number = 3;
+// let num2 : number = 2;
+// let card : number = num1 / num2 
+// console.log(card)
+// let num1 : number = 20;
+// let num2 : number = 4;
+// let card : number = num1 / num2 
+// console.log(card)
+// let bags : number = 20;
+// let apples : number = 5;
+// let sum : number = bags / apples 
+//  console.log(sum);
+// ========> Concatenation
+// Error in /
+//  let bags  = "20";
+// let apples  = 5;
+// let sum = bags / apples  // error
+//  console.log(sum);
+// error in *
+// let bags  = "20";
+// let apples  = 5;
+// let sum = bags * apples  // error
+//  console.log(sum);
+// error in subtraction
+// let bags  = "20";
+// let apples  = 5;
+// let sum = bags - apples  // error
+//  console.log(sum);
+// Concatenation in Addition
+// let bags  = "20";
+// let apples  = 5;
+// let sum = bags + apples  
+//  console.log(sum);
+// =======> Exponentiation
+// let num1 : number = 5;
+// let num2 : number = 5;
+// let sum : number = num1 ** num2;
+// console.log(sum)
+// let num1 : number = 3;
+// let num2 : number = 3;
+// let sum : number = num1 ** num2;
+// console.log(sum)
+// =======> Modulus (%)  Remainder
+// let apples = 5
+// let bags = 2
+// let reminder = apples % bags
+// console.log(reminder)
+// let apples = 10
+// let bags = 4
+// let reminder = apples % bags
+// console.log(reminder)
+// ===========> Unary Operators 
+// ** post increment
+// let a : number = 5;
+// let b : number = 2;
+// console.log(a);
+// console.log(a++); // 5 (Post increment pehly value return kry ga us k bd 1 plus kry ga)
+// console.log(a); // 6
+// console.log(a);
+// ** Pre increment 
+// let a : number = 5;
+// let b : number = 2;
+// console.log(a); // 5
+// console.log(++a); // 6 (Pre Increment Phly add kry ga us k bd value ko return kry ga)
+// console.log(a); // 6
+// *** Post Decrement 
+// let a : number = 5;
+// let b : number = 2;
+// console.log(a--); // 5 (Post Decrement: phly return kry ga us k bd value ko subtract (-) kry ga)
+// console.log(a); // 4
+// *** Pre Decrement 
+// let a : number = 5;
+// let b : number = 2;
+// console.log(a); // 5
+// console.log(--a); // 4 ( Pre Decrement : Phly value ko subtract kry ga us k bd return kry ga)
+// console.log(a); // 4
+var num1 = 5;
+var num2 = 6;
+var pre = ++num1;
+var post = num1++;
+console.log(pre);
+console.log(post);
+console.log(num1);
+// let x : number = 5;
+// let y : number = 2;
+// let z : number;
+// z =
+//  ++x // 6
+// +
+//  x++ // 6 ===> 7
+// + 
+//  --y  // 1
+// +
+//  y--  // 1 ===> 0
+// +
+//  x // 7
+// +
+//  y++ // 0 ===> 1
+// +
+//  y; // 1
+// console.log(z);
