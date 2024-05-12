e70efc39225c911c50d5203de6d24dd6564693d1
M Nasir Khan <immnk97@gmail.com>
2024-05-12 23:57:45 +0500

    Day 07

---
+++ Class07/index.ts
@@ -0,0 +1,131 @@
+// let age = 18;
+
+// import { log } from "console";
+
+// console.log(age > 18);
+
+// let age = 19;
+
+// console.log(age > 18);
+
+
+// let age = 18;
+
+// console.log(age > 18, 'University');
+// console.log(18 < age, 'School');
+
+
+// let age = 18;
+
+// console.log(age >= 18, 'University');
+// console.log(18 <= age, 'School');
+
+
+// let num = (2 + 3 * ((8 + 4 / 2) * 3));
+
+// console.log(num);
+
+// let num = (2 + 3 * ((8 + 4 / 2) * 5));
+
+// console.log(num);    // BODMAS
+
+// let num1 = 5;
+// let num2 = 17;
+
+// console.log( num1 % num2);
+
+// let num1 = 5;   
+// let num2 = 17;
+
+// console.log( num2 % num1);
+
+// ========> Assignment Operator (=)
+
+// let c = 10;
+
+// c += 5;
+
+// console.log(c);
+
+
+
+// ======> Comparision OPerators
+
+// 5 == 2 is false ====> equal to
+// 5 != 2 is true   ====> not equal to
+// 5 > 2 is true
+// 5 < 2 is false
+
+
+// let a = 5;
+// let b = 2;
+
+// let isEqual = (a == b); // false
+// let isNotEqual = (a != b); // true
+// let isGreaterThan = ( a > b); // true
+// let isLessThan = ( a < b); // false
+
+// console.log(isEqual, "isEqual")
+// console.log(isNotEqual, "isNotEqual")
+// console.log(isGreaterThan, "isGreaterThan")
+// console.log(isLessThan, "isLessThan")
+
+
+// =====> Logical Operators
+
+// (5 > 0) && (2 > 0) true
+// (5 < 0) || (2 > 0 ) True
+// !(5 > 0) false
+
+
+// let a = 5;
+// let b = 2;
+
+// let logicalAnd = (a > 0) && (b > 0)  // true
+// console.log(logicalAnd);
+
+// let logicalOr = (a < 0) || (b > 0);  // true
+// console.log(logicalOr)
+
+// let logicalNot = !(a > 0);  // false;
+// console.log(logicalNot);
+
+
+
+// =======> Logical Statement
+
+// If and If-Else Statements
+
+// if (faisla lena) {agr faisla theek hwa toh us pr amal krna}
+
+
+
+// let month = "Ramadan";
+
+// if(month == "Ramadan"){
+//     console.log("Water bottle is not allowed") // chaly ga 
+// }
+
+// if(month == "ramadan"){
+//     console.log("Water bottle is not allowed") // nh chaly ga kiun k variable me "Ramadan" h or nichy "ramadan"
+// }
+
+
+
+
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("ID card Ban skta hai")
+// }
+
+
+// let age = 17;
+
+// if(age > 18){
+//     console.log("ID card Ban skta hai")
+// }else{
+//     console.log("ID card nh Ban skta",)
+// }
+
