41ecdb281ed790d97732eb1a360605478f8649d1
M Nasir Khan <immnk97@gmail.com>
2024-05-14 15:42:11 +0500

    Day 08

---
+++ Class08/index.ts
@@ -0,0 +1,30 @@
+// ====>  function
+
+// function khana(){
+//     console.log("Khana ready hai")
+// }
+
+// khana()
+
+
+// function pani(){
+//     console.log("Ye lijiye Pani")
+// }
+
+// pani()
+
+
+
+
+// function foo(){
+//     let myName = "Nasir";
+//     let myCourse = "AI & Meta verse"
+//     console.log("inside func ===>", myName)
+
+// }
+
+// // console.log("outside func", myName)
+
+
+// foo()
+
